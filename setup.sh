c78afec90c2f3d735a11f9d1023f5b88c84eb92a
hendrakyle99 <156536300+hendrakyle99@users.noreply.github.com>
2024-04-03 14:25:05 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,8 +21,8 @@ download_files()
 {
     	clear
   	echo "Downloading files from repo hilink mak cling..."
-   	wget -O $DIR/m https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/m && chmod +x $DIR/m
- 	wget -O $DIR/hilink https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/hilink && chmod +x $DIR/hilink
+   	wget -O $DIR/m https://raw.githubusercontent.com/hendrakyle99/menghilink/main/usr/bin/m && chmod +x $DIR/m
+ 	wget -O $DIR/hilink https://raw.githubusercontent.com/hendrakyle99/menghilink/main/usr/bin/hilink && chmod +x $DIR/hilink
   	wget -O $DIR/balong-nvtool https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/balong-nvtool
    	wget -O $DIR/updatehilink https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/balong-nvtool && chmod +x $DIR/updatehilink
     	finish
